28cc9d7c1f186eba198070736c01ac0b00923055
Kamil Butryn <kaybenot@gmail.com>
2024-12-01 15:02:09 +0100

    fixes

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -27,7 +27,7 @@ public class Interactor : MonoBehaviour
         }
 
         var cam = Camera.main;
-        var ray = cam.ScreenPointToRay(new Vector3(screenX * (640f / Screen.width), screenY * (360f / Screen.height)));
+        var ray = cam.ScreenPointToRay(new Vector3(screenX, screenY));
         Debug.DrawRay(ray.origin, ray.direction, Color.blue, 30f);
 
         if (Physics.Raycast(ray, out var hit, float.PositiveInfinity, interactableLayer))
